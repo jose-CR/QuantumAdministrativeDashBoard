7302a5877d91c57f1938a95f238ecb593cd1cd15
Alejandro <hjosealejandro@gmail.com>
2026-06-05 17:13:03 -0600

    feat: :sparkles: implementacion de los modelos cliente y referencia

---
+++ app/Models/Clients.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Clients extends Model
+{
+    /** @use HasFactory<\Database\Factories\ClientsFactory> */
+    use HasFactory;
+
+    public function references()
+    {
+        return $this->hasMany(Reference::class);
+    }
+}
---
+++ database/migrations/2026_06_05_222334_create_references_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('references', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('client_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->enum('reference_type', [
+                'family',
+                'friend',
+            ]);
+
+            $table->string('full_name');
+
+            $table->string('relationship')->nullable();
+
+            $table->string('phone');
+
+            $table->string('address')->nullable();
+
+            $table->string('occupation')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('references');
+    }
+};
