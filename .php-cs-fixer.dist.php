a28a10dfc8a3c1d7f274ca62063de4ef3b74887d
Alejandro <hjosealejandro@gmail.com>
2026-06-12 09:49:07 -0600

    ci(settings): :building_construction: implementacion de php cs fixer
    fixer se agrego para tener mas limpio el codigo y tenerlo mas ordenado

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,31 @@
+<?php
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+return (new Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@auto' => true,
+        '@auto:risky' => false,
+        '@PhpCsFixer' => false,
+        '@PSR12' => true,
+
+        'declare_strict_types' => false,
+    ])
+    // 💡 by default, Fixer looks for `*.php` files excluding `./vendor/` - here, you can groom this config
+    ->setFinder(
+        (new Finder())
+            // 💡 root folder to check
+            ->in(__DIR__)
+        // 💡 additional files, eg bin entry file
+        // ->append([__DIR__.'/bin-entry-file'])
+        // 💡 folders to exclude, if any
+        // ->exclude([/* ... */])
+        // 💡 path patterns to exclude, if any
+        // ->notPath([/* ... */])
+        // 💡 extra configs
+        // ->ignoreDotFiles(false) // true by default in v3, false in v4 or future mode
+        // ->ignoreVCS(true) // true by default
+    )
+;
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -6,6 +6,7 @@ use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Navigation\NavigationGroup;
 use Filament\Pages\Dashboard;
 use Filament\Panel;
 use Filament\PanelProvider;
@@ -18,8 +19,6 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
-use Filament\Navigation\NavigationGroup;
-use Filament\Support\Icons\Heroicon;
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -60,6 +59,7 @@ class AdminPanelProvider extends PanelProvider
             ->navigationGroups([
                 NavigationGroup::make()
                     ->label('Administración'),
-            ]);
+            ])
+        ;
     }
 }
